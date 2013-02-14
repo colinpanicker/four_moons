b570e817e80bc1ab4cb34935d41aca89894628a9
colin <colinkp@gmail.com>
2013-02-14 23:31:28 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourMoons::Application.config.secret_token = '19cdfce2bb093de52f6583fe684de86aa3c80a95d3f2677e5fbcb7f97b4063a68fd59b8d7e962fe98eb523446acabed3159f7be6bc755dd64a1ad6107e8b3f0f'
